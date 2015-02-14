9c8dffd3d65bb18dec91dce105b8c2fb663f81b6
Руслан <ahmetsharipov.ruslan@gmail.com>
2015-02-14 20:09:27 +0400

    oups

---
+++ UnderWater/Assets/scripts/PlatformMove.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformMove : MonoBehaviour
+{
+
+    public Transform[] part;
+    public Transform player;
+    private Vector3 playercurrentposition;
+    // Use this for initialization
+    void Start()
+    {
+        playercurrentposition = player.transform.position;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        for (int i = 0; i < part.Length; i++)
+        {
+            Vector3 temp = player.transform.position - playercurrentposition + part[i].transform.position;
+            part[i].transform.position = new Vector3(temp.x,part[i].transform.position.y,temp.z);
+            playercurrentposition = player.transform.position;
+        }
+    }
+}
--- UnderWater/Assets/scripts/WaterControl.cs
+++ UnderWater/Assets/scripts/WaterControl.cs
@@ -4,42 +4,49 @@ using System.Collections;
 public class WaterControl : MonoBehaviour
 {
 
-		public Light light;
-		private bool watCon = true;
-		public GlobalFog fog;
-		public DepthOfFieldScatter dfs;
-		void Start ()
-		{
-		}
+    public Light light;
+    private bool watCon = true;
+    public DepthOfFieldScatter dfs;
+    void Start()
+    {
 
-		void OnTriggerEnter (Collider oth)
-		{
-				if (oth.tag == "Water") {
-						if (watCon) {
-								fog.enabled = false;	
-								watCon = false;
-								light.enabled = false;
-				dfs.enabled=false;
-						} else
-		if (!watCon) {
-								fog.enabled = true;
-								watCon = true;
-								light.enabled = true;
-				dfs.enabled=true;
-						}
-				}
-		}
+    }
 
-		void OnTriggerExit (Collider oth)
-		{
-				if (oth.tag == "Water")
-				if (oth.transform.position.y >= this.transform.position.y) {
-						watCon = true;
-						fog.enabled = true;
-				} else {
-						watCon = false;
-						fog.enabled = false;
-				}
-					
-		}
+    void OnTriggerEnter(Collider oth)
+    {
+        if (oth.tag == "Water")
+        {
+            if (watCon)
+            {
+                RenderSettings.fog = false;
+                watCon = false;
+                light.enabled = false;
+                dfs.enabled = false;
+            }
+            else
+                if (!watCon)
+                {
+                    RenderSettings.fog = true;
+                    watCon = true;
+                    light.enabled = true;
+                    dfs.enabled = true;
+                }
+        }
+    }
+
+    void OnTriggerExit(Collider oth)
+    {
+        if (oth.tag == "Water")
+            if (oth.transform.position.y >= this.transform.position.y)
+            {
+                watCon = true;
+                RenderSettings.fog = true;
+            }
+            else
+            {
+                watCon = false;
+                RenderSettings.fog = false;
+            }
+
+    }
 }
